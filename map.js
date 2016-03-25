86c2f54d006dd47ee812fe7077331403ef13c809
thomas1903 <tomandrewmills@gmail.com>
2016-03-25 15:52:33 +0000

    changed index.html

---
+++ map.js
@@ -0,0 +1,17 @@
+/**
+ * Created by 1403198 on 19/02/2016.
+ */
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0, lng: 0},
+        zoom: 3,
+        styles: [{
+            featureType: 'poi',
+            stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+        }, {
+            featureType: 'transit.station',
+            stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+        }],
+        disableDoubleClickZoom: true
+    });
+}
