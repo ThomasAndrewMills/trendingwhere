a5cfb97dd0bf0e9c8c6a25c7658ce9a52504c305
thomas1903 <tomandrewmills@gmail.com>
2016-04-15 17:41:58 +0100

    .

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
                         $connection = getConnectionWithAccessToken("455303569-USh4o67B8c6vYzw8taIp4G6WtjQoxpMOS8rseRNI", "lIrlNxvTW6TY5DBUJmn7VuiWlMtAPz4tUrLE5ngUpENRL");
                         $content = $connection->get("statuses/home_timeline");
 
-                        echo($content);
+                        echo($content[1]);
                         ?>
                         Sign in using your twitter account and explore the trending topics<br> of cities around the world!
                     </span>
