9f818a8870d395148ffa263e757479bcd575123c
thomas1903 <tomandrewmills@gmail.com>
2016-04-15 18:07:28 +0100

    .

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
                         }
 
                         $connection = getConnectionWithAccessToken("455303569-USh4o67B8c6vYzw8taIp4G6WtjQoxpMOS8rseRNI", "lIrlNxvTW6TY5DBUJmn7VuiWlMtAPz4tUrLE5ngUpENRL");
-                        $content = $connection->POST("oauth/request_token");
+                        $content = $connection->POST("oauth/access_token");
 
 
                         print_r($content);
