6c527c78253d957f2e96cc0885be236faae61474
thomas1903 <tomandrewmills@gmail.com>
2016-04-19 11:23:32 +0100

    .

--- twitterSetup.php
+++ twitterSetup.php
@@ -22,4 +22,9 @@
 
     $_SESSION['access_token'] = $access_token;
 
+    $access_token = $_SESSION['access_token'];
+
+    $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+    $user = $connection->get("account/verify_credentials");
 ?>
