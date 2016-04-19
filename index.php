55561c82633ec2f51bd05da1699fc2367140350d
thomas1903 <tomandrewmills@gmail.com>
2016-04-19 09:45:15 +0100

    .

--- index.php
+++ index.php
@@ -60,11 +60,11 @@
                         $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
 
                         $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $_REQUEST['oauth_verifier']]);
-
+                        echo("test");
                         $_SESSION['access_token'] = $access_token;
 
                         $access_token = $_SESSION['access_token'];
-                        echo("test");
+
                         $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
                         $user = $connection->get("account/verify_credentials");
