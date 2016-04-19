87e00e1267f152f225fa77306e7b5c1f390df7c0
thomas1903 <tomandrewmills@gmail.com>
2016-04-19 09:42:10 +0100

    .

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                         $_SESSION['oauth_token'] = $request_token['oauth_token'];
                         $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
                         $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
-
+echo("test");
                         define('CONSUMER_KEY', getenv('CONSUMER_KEY'));
                         define('CONSUMER_SECRET', getenv('CONSUMER_SECRET'));
                         define('OAUTH_CALLBACK', getenv('OAUTH_CALLBACK'));
