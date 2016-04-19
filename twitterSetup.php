c442b714103cef1c7f3d64437a55020250157bd9
thomas1903 <tomandrewmills@gmail.com>
2016-04-19 10:44:09 +0100

    .

--- twitterSetup.php
+++ twitterSetup.php
@@ -11,9 +11,9 @@ use Abraham\TwitterOAuth\TwitterOAuth;
     $request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
 
     if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
-        // Abort! Something is wrong.
+        echo("test");
     }
-echo("test");
+
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
 
     $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $_REQUEST['oauth_verifier']]);
