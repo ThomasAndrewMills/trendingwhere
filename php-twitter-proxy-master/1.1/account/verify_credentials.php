ab403748bcb2d9053ad3297ccd63e769b59fdf5f
1403198 <c.robertson97@rgu.ac.uk>
2016-04-22 13:46:08 +0100

    test

--- php-twitter-proxy-master/1.1/account/verify_credentials.php
+++ php-twitter-proxy-master/1.1/account/verify_credentials.php
@@ -8,6 +8,6 @@ session_start();
  
 require '../../twitter-proxy.php';
 
-Proxy::relay( 'C:\Users\1403198\PhpstormProjects\trendingwhere1\php-twitter-proxy-master\1.1\account\verify_credentials.php' );
+Proxy::relay( '1.1/account/verify_credentials' );
 
 ?>
