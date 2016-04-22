945c6b5411ae8b0a3b94edf8c4a049e0f1f5f6cc
1403198 <c.robertson97@rgu.ac.uk>
2016-04-22 14:49:35 +0100

    nothing

--- php-twitter-proxy-master/1.1/account/verify_credentials.php
+++ php-twitter-proxy-master/1.1/account/verify_credentials.php
@@ -3,8 +3,8 @@
  * account/verify_credentials
  * https://dev.twitter.com/docs/api/1.1/get/account/verify_credentials
  */
- 
+echo '<p>Testing</p>';
 require '../../twitter-proxy.php';
-
+echo '<p>Testing</p>';
 Proxy::relay( 'account/verify_credentials' );
 ?>
