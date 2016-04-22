aa4b1553606e89ed7708223ab7dabaf3acaef593
thomas1903 <tomandrewmills@gmail.com>
2016-04-22 13:51:07 +0100

    Added another twitter API thing

--- php-twitter-proxy-master/config.php
+++ php-twitter-proxy-master/config.php
@@ -10,32 +10,6 @@ session_start();
 // Failures result in immediate HTTP error response.
 
 
-// Restrict permitted HTTP methods.
-// It's recommended to remove POST support if your proxy is public.
-Proxy::match_methods('GET,POST' );
-
-
-// Restrict permitted HTTP Referrers.
-// This is simply designed to prevent others using your proxy from JavaScript. The referrer is easily forged via other means.
-Proxy::match_referrer('!^https?://(?:localhost|mydomain\.com)/!');
-
-
-// Restrict permitted HTTP Origin headers.
-// Similar to above, but specifically for Ajax requests.
-Proxy::match_origin('!^https?://(?:localhost|mydomain\.com)!');
-
-
-// Restrict permitted remote IP addresses
-// This is pointless if using with JavaScript.
-Proxy::match_remote_addr('/^(127/.0/.0/.1|192/.168/.0/.\d+)$/');
-
-
-
-
-// Global security checks have passed.
-// The following configures the Twitter client for proxying request
-
-
 // Twitter application key and secret
 // See: https://dev.twitter.com/apps 
 Proxy::init_client( 'OAtCFNvjagzQCoHp5sJU2KtuE', 'CXHT54PYFdFugE6LKaqvVhUeFRZr8BVhZyHN8iGEsZmfqBlBBZ' );
@@ -46,13 +20,3 @@ Proxy::init_client( 'OAtCFNvjagzQCoHp5sJU2KtuE', 'CXHT54PYFdFugE6LKaqvVhUeFRZr8B
 // Obtaining an access token is beyond the scope of this library.
 // You could pull them from a database, or send the user through an OAuth flow, or just hard code them.
 Proxy::auth_client('455303569-USh4o67B8c6vYzw8taIp4G6WtjQoxpMOS8rseRNI', 'lIrlNxvTW6TY5DBUJmn7VuiWlMtAPz4tUrLE5ngUpENRL');
-
-
-// Lock screen_name and user_id parameters in some API calls.
-// This prevents other people using some endpoints for their own Twitter feeds
-Proxy::lock_users( array( 'your screen_name' => 'your user_id' ) );
-
-
-// caching engine - currently only APC supported and is enabled by default
-Proxy::enable_cache( 'apc', 'your_prefix' );
-// Proxy::disable_cache();
