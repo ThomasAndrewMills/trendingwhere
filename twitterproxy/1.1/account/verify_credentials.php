32ebf73c24ab12c187855af88e174ad515a75d52
1403198 <c.robertson97@rgu.ac.uk>
2016-04-22 16:18:20 +0100

    nothing

--- twitterproxy/1.1/account/verify_credentials.php
+++ twitterproxy/1.1/account/verify_credentials.php
@@ -7,9 +7,28 @@
 require '../../twitter-proxy.php';
 
 Proxy::relay( 'account/verify_credentials' );
+
+$token=$_GET["oauth_token"];
+$verifier=$_GET["oauth_verifier"];
+
 ?>
 
 <script>
+
+    function getTrend()
+    {
+        var url="https://api.twitter.com/1.1/trends/place.json";
+        var data={"param1": "value1",
+                    "param2":"value2"
+                };
+        $.getJSON(url,data,
+                    function(reply){
+
+                    }
+                   );
+
+    } //end function
+
     function aFunction(){
         print("Test");
     }
