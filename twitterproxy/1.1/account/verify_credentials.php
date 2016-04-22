be827dfc6983284cfefbdc3b1a4f82adc556c7da
1403198 <c.robertson97@rgu.ac.uk>
2016-04-22 16:38:21 +0100

    nothing

--- twitterproxy/1.1/account/verify_credentials.php
+++ twitterproxy/1.1/account/verify_credentials.php
@@ -6,7 +6,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-echo"here";
+//echo"here";
 require '../../twitter-proxy.php';
 
 Proxy::relay( 'account/verify_credentials' );
