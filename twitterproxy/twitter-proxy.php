dee7d5694b163a7cb83fc17b2755ea1346b3043a
Thomas <tomandrewmills@gmail.com>
2016-04-23 15:01:58 +0100

    Testing twitter proxy

--- twitterproxy/twitter-proxy.php
+++ twitterproxy/twitter-proxy.php
@@ -286,6 +286,7 @@ abstract class Proxy {
      * @param string regexp pattern to match against HTTP Origin header
      */
     public static function match_origin( $pattern ){
+        echo($pattern);
         if( empty($_SERVER['HTTP_ORIGIN']) ){
             self::fatal( 400 , 'Empty origin' );
         }
