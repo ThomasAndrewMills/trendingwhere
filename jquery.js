31a74e5c460fa6b016f64e6b6b3c4c874f252ed2
Thomas <tomandrewmills@gmail.com>
2016-04-24 15:36:35 +0100

    Now fully working with the twitter api.

--- jquery.js
+++ jquery.js
@@ -23,25 +23,25 @@ function initialize() {
     //pop up trends menu
     var content =       '<div class="trendsMenuContainer">' +
         '<div class="cityTitle">' +
-        'CITY NAME' +
+        '' +
         '</div>' +
         '<div class="trendsGroup">' +
-        '<div class="trend">Trend1</div>' +
-        '<div class="trend">Trend2</div>' +
-        '<div class="trend">Trend3</div>' +
-        '<div class="trend">Trend4</div>' +
+        '<div class="trend"></div>' +
+        '<div class="trend"></div>' +
+        '<div class="trend"></div>' +
+        '<div class="trend"></div>' +
         '</div>'+
         '<div class="trendsGroup">' +
-        '<div class="trend">Trend5</div>' +
-        '<div class="trend">Trend6</div>' +
-        '<div class="trend">Trend7</div>' +
-        '<div class="trend">Trend8</div>' +
+        '<div class="trend"></div>' +
+        '<div class="trend"></div>' +
+        '<div class="trend"></div>' +
+        '<div class="trend"></div>' +
         '</div>'+
         '<div class="trendsGroup">' +
-        '<div class="trend">Trend9</div>' +
-        '<div class="trend">Trend10</div>' +
-        '<div class="trend">Trend11</div>' +
-        '<div class="trend">Trend12</div>' +
+        '<div class="trend"></div>' +
+        '<div class="trend"></div>' +
+        '<div class="trend"></div>' +
+        '<div class="trend"></div>' +
         '</div>' +
         '</div>';
 
