a29afd373308cc8f1012824d8f9b704adebfb59c
1403198 <c.robertson97@rgu.ac.uk>
2016-04-28 16:55:08 +0100

    button

--- index.php
+++ index.php
@@ -1,73 +1,73 @@
 <html>
-    <head>
-        <title>
-            trendingwhere?
-        </title>
-        <!--CSS link-->
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <!--Google Maps API link-->
-        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.20"></script>
+<head>
+    <title>
+        trendingwhere?
+    </title>
+    <!--CSS link-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!--Google Maps API link-->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.20"></script>
 
-        <!-- Use jquery script -->
-        <script type="text/javascript" src="jquery.js"></script>
+    <!-- Use jquery script -->
+    <script type="text/javascript" src="jquery.js"></script>
 
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
-        <!--Importing fonts from Google-->
-        <link href='https://fonts.googleapis.com/css?family=Oleo+Script:400,700' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Bitter:700' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-        <div id="overlay">
+    <!--Importing fonts from Google-->
+    <link href='https://fonts.googleapis.com/css?family=Oleo+Script:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Bitter:700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div id="overlay">
 
-        </div>
-        <div id="infoContainer">
-            <span class="smallTitle">#trendingWhere?</span>
-            <div id="info">
-                <h3 style="margin:0px;font-size:30px;">How to use</h3>
-                <p style="font-size:13px;">
-                    Start by selecting a red marker over a city.
-                </p>
+</div>
+<div id="infoContainer">
+    <span class="smallTitle">#trendingWhere?</span>
+    <div id="info">
+        <h3 style="margin:0px;font-size:30px;">How to use</h3>
+        <p style="font-size:13px;">
+            Start by selecting a red marker over a city.
+        </p>
 
-                <p style="font-size:13px;">
-                    Click on each trend to search for it on Twitter.
-                </p>
-                <p style="font-size:13px;">
-                    The amount of tweets about each trend is also displayed.
-                </p>
-                <p style="font-size:13px;">
+        <p style="font-size:13px;">
+            Click on each trend to search for it on Twitter.
+        </p>
+        <p style="font-size:13px;">
+            The amount of tweets about each trend is also displayed.
+        </p>
+        <p style="font-size:13px;">
 
-                </p>
-                <p style="font-size:13px;">
-                    Developed by Thomas Mills, Darren Williamson, Ciaran Miller, Callum Melia, Callum Robertson
-                </p>
-            </div>
+        </p>
+        <p style="font-size:13px;">
+            Developed by Thomas Mills, Darren Williamson, Ciaran Miller, Callum Melia, Callum Robertson
+        </p>
+    </div>
 
-        </div>
-        <div id="map-canvas">
-        </div>
-        <div id="mainMenu">
-            <div id="menuItem1">
-                <span class="title">#trending<span class="titleColourEdit">Where?</span></span>
-                <br>
+</div>
+<div id="map-canvas">
+</div>
+<div id="mainMenu">
+    <div id="menuItem1">
+        <span class="title">#trending<span class="titleColourEdit">Where?</span></span>
+        <br>
                 <span class="subtitle">
                     <script>
 
                         $(document).ready(function () {
-                            $(".signintwitter").click(function () {
+                            $(".start").click(function () {
                                 $('#overlay').hide();
                                 $('#mainMenu').hide();
                                 $('#overlay2').show();
                                 $('#infoContainer').show();
                             });
                         });
-                        </script>
+                    </script>
                         Sign in using your twitter account and explore the trending topics<br> of cities around the world!
                     </span>
-                <br>
-                <br>
-                <img src="trendingWhereInterface.png" class="trendingWhereInterface">
+        <br>
+        <br>
+        <img src="trendingWhereInterface.png" class="trendingWhereInterface">
                 <span class="bulletPointText">
                     <ul>
                         <li>Pan across the map by clicking and dragging your mouse.</li>
@@ -77,7 +77,7 @@
                     </ul>
                     <a href='#' class='start'>Begin</a>
                 </span>
-            </div>
-        </div>
-    </body>
+    </div>
+</div>
+</body>
 </html>
