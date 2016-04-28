513b3852802c8dba1d40369923429aaf320fcbd5
1403198 <c.robertson97@rgu.ac.uk>
2016-04-28 17:38:35 +0100

    image

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
                     </span>
         <br>
         <br>
-        <img src="map.jpg" class="trendingWhereInterface">
+        <img src="map.png" class="trendingWhereInterface">
                 <span class="bulletPointText">
                     <ul>
                         <li>Pan across the map by clicking and dragging your mouse.</li>
