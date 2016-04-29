3274390b15f794606eb0a4e6f03bc620be8daa86
thomas1903 <tomandrewmills@gmail.com>
2016-04-29 09:11:28 +0100

    Changing the start button design

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
                         <li>The top 12 trending topics for that city will be displayed.</li>
                         <li>Get started by clicking the button below!</li>
                     </ul>
-                    <a href="#" class='start'>Start</a>
+                    <a href="#" class='start'><span class="button">Start</span></a>
                 </span>
     </div>
 </div>
